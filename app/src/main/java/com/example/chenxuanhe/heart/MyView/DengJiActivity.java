d396c16882d0f280115542012d2b1f9c17f0bcdd
chenxuanhe77 <136906810@qq.com>
2016-11-02 14:53:50 +0800

    黑屏 先提交。

--- app/src/main/java/com/example/chenxuanhe/heart/MyView/DengJiActivity.java
+++ app/src/main/java/com/example/chenxuanhe/heart/MyView/DengJiActivity.java
@@ -3,6 +3,9 @@ package com.example.chenxuanhe.heart.MyView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.chenxuanhe.heart.R;
@@ -26,8 +29,25 @@ public class DengJiActivity extends AppCompatActivity {
     public void setTitle(){
         toolbar = (Toolbar) findViewById(R.id.Tool_bar_my_dengji);
         center_title = (TextView) findViewById(R.id.centertitle_dengji);
+        toolbar.setNavigationIcon(R.drawable.chat);
         setSupportActionBar(toolbar);
         setTitle("");
         center_title.setText("我的等级");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DengJiActivity.this.finish();
+            }
+        });
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
