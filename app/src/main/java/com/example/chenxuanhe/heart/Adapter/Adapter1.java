eeebb627c65dcf97bd7b03017504e43edc312e31
Xuanhe Ch <136906810@qq.com>
2016-12-04 17:01:25 +0800

    解决点赞关注，开始考虑联网

--- app/src/main/java/com/example/chenxuanhe/heart/Adapter/Adapter1.java
+++ app/src/main/java/com/example/chenxuanhe/heart/Adapter/Adapter1.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chenxuanhe.heart.R;
 import com.example.chenxuanhe.heart.Util.Gongju;
@@ -22,6 +24,8 @@ public class Adapter1 extends RecyclerView.Adapter<Adapter1.MyViewHolder> {
 
     private Context mContext;
 
+    private boolean flag;//点击关注判断
+
 
     public Adapter1(Context context, List<Gongju> datas){
         this.mContext = context;
@@ -37,9 +41,36 @@ public class Adapter1 extends RecyclerView.Adapter<Adapter1.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
         Gongju p = message.get(position);
         holder.msg.setText(p.txtmessage);
+        //点击关注，触发事件操作
+        //利用flag 直接判断，不再需要累加奇偶计算
+        holder.attention.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag){
+                    Toast.makeText(mContext, "取消关注~变心了~", Toast.LENGTH_SHORT).show();
+                    holder.attention.setImageResource(R.drawable.disattention);
+                    holder.attention.invalidate();
+                    flag =false;
+                }else {
+                    Toast.makeText(mContext, "关注了呦！尽在关注中", Toast.LENGTH_SHORT).show();
+                    holder.attention.setImageResource(R.drawable.attention);
+                    holder.attention.invalidate();
+                    flag = true;
+                }
+            }
+        });
+        //点击点赞，触发事件操作
+        holder.goodjob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //点赞之后，数字增加
+                Toast.makeText(mContext, "尽在与我相关中~", Toast.LENGTH_SHORT).show();
+                    holder.goodjob_number.setText(" "+ 1);
+            }
+        });
 
     }
 
@@ -54,11 +85,19 @@ public class Adapter1 extends RecyclerView.Adapter<Adapter1.MyViewHolder> {
     public  class  MyViewHolder extends RecyclerView.ViewHolder{
 
         public TextView msg;
+        public ImageView attention;
+        public ImageView goodjob;
+        public TextView goodjob_number;
 
         public MyViewHolder(View v) {
             super(v);
             msg=(TextView) v.findViewById(R.id.txt_cardmessage);
+            attention = (ImageView) v.findViewById(R.id.card_attention);//关注image
+            goodjob = (ImageView) v.findViewById(R.id.card_goodjob);    //点赞image
+            goodjob_number = (TextView) v.findViewById(R.id.goodjob_number);//点赞数字text
         }
 
     }
+
+
 }
