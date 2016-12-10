8a74bb92c812a4cf0bdf7026d4e8ca35c83b5ff1
Xuanhe Ch <136906810@qq.com>
2016-12-10 17:27:44 +0800

    完美解决popupwindow及上啦显示软键盘

--- app/src/main/java/com/example/chenxuanhe/heart/CardViewActivity.java
+++ app/src/main/java/com/example/chenxuanhe/heart/CardViewActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +35,8 @@ public class CardViewActivity extends AppCompatActivity {
     private TextView card_goodjob_number;
     private EditText edit_card;
     private CardView card_reply;
+    private PopupWindow mpopupWindow;
+    private CardView card_commit;
 
 
     @Override
@@ -38,13 +47,67 @@ public class CardViewActivity extends AppCompatActivity {
         setTitle();
     }
 
+    private void showPopupWindow(){
+        View content = LayoutInflater.from(CardViewActivity.this).inflate(R.layout.popuplayout,null);
+
+        card_commit = (CardView) content.findViewById(R.id.at_card_commit);
+        edit_card = (EditText) content.findViewById(R.id.at_card_edit);
+
+        edit_card.setImeOptions(EditorInfo.IME_ACTION_SEND);
+        edit_card.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_SEND){
+                    Toast.makeText(CardViewActivity.this, "ddddddddd", Toast.LENGTH_SHORT).show();
+                    card_commit.performClick();
+                }
+                return false;
+            }
+        });
+
+
+        mpopupWindow = new PopupWindow(content, ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,true);//content是子布局，pppwindow的布局
+        mpopupWindow.setContentView(content);
+        View mainview = LayoutInflater.from(CardViewActivity.this).inflate(R.layout.activity_card_view,null);
+        //mainview才是cardview的主布局，在这个上面显示pppwindow
+        mpopupWindow.setAnimationStyle(R.style.reply_menu_enter_exit);
+        content.clearAnimation();
+
+        //防止PopupWindow被软件盘挡住
+        mpopupWindow.setSoftInputMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mpopupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+
+        mpopupWindow.showAtLocation(mainview, Gravity.CENTER_VERTICAL,0,0);
+
+
+        card_commit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CardViewActivity.this, "daadadadad", Toast.LENGTH_SHORT).show();
+            }
+        });
+        edit_card.setVisibility(View.VISIBLE);
+        edit_card.setFocusable(true);
+        edit_card.setFocusableInTouchMode(true);
+        edit_card.requestFocus();
+        edit_card.requestFocusFromTouch();//对应的View支持Focus，不支持在Touch模式下的Focus中也行获取焦点
+        //三句一起，在这才能显示软键盘，一般1.2局就能显示
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInputFromInputMethod(edit_card.getWindowToken(), 0);
+        imm.toggleSoftInput(100, InputMethodManager.HIDE_NOT_ALWAYS);//切换软键盘的显示与隐藏
+
+    }
+
+    /**
+     * 初始化绑定card activity的控件，设置点赞，关注，回复事件
+     */
     public void init() {
         card_msg = (TextView) findViewById(R.id.at_card_msg);
         card_attention = (ImageView) findViewById(R.id.at_card_attention);
         card_goodjob = (ImageView) findViewById(R.id.at_card_goodjob);
         card_goodjob_number = (TextView) findViewById(R.id.at_goodjob_number);
-        edit_card = (EditText) findViewById(R.id.at_card_edit);
-        edit_card.setVisibility(View.GONE);
         card_reply = (CardView) findViewById(R.id.at_card_reply);
 
         Intent intent = getIntent();
@@ -69,17 +132,11 @@ public class CardViewActivity extends AppCompatActivity {
         card_reply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edit_card.setVisibility(View.VISIBLE);
-                edit_card.setFocusable(true);
-                edit_card.setFocusableInTouchMode(true);
-                edit_card.requestFocus();
-                edit_card.requestFocusFromTouch();//对应的View支持Focus，不支持在Touch模式下的Focus中也行获取焦点
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.showSoftInput(edit_card, 0);
+                showPopupWindow();
             }
         });
 
-        edit_card.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+     /*   edit_card.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
                 Toast.makeText(CardViewActivity.this, "ddddddddd", Toast.LENGTH_SHORT).show();
@@ -94,11 +151,9 @@ public class CardViewActivity extends AppCompatActivity {
                     imm.toggleSoftInput(0,InputMethodManager.HIDE_NOT_ALWAYS);
                 }
             }
-        });
+        });*/
 
     }
-
-
 
 
 
